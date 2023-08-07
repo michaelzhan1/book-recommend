33bf6ace2add90fb37cf05776ae5a57d4b3a35d9
Michael Zhan <michaelzhan2001@gmail.com>
2023-08-07 13:46:15 -0500

    Add debounce

--- components/Search.js
+++ components/Search.js
@@ -2,40 +2,47 @@
 
 
 import { useState } from 'react';
+import _debounce from 'lodash.debounce';
 
 
 
 export default function Search (props) {
   const [res, setRes] = useState([]);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
 
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const query = (e.target.query.value).replaceAll(' ', '+');
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${process.env.NEXT_PUBLIC_API_KEY}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    setRes(data.items.slice(0, 3));
+  const handleSearch = async (input) => {
+    setLoading(true);
+    try {
+      const url = `https://www.googleapis.com/books/v1/volumes?q=${input}&projection=lite&key=${process.env.NEXT_PUBLIC_API_KEY}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      setRes(data.items.slice(0, 5));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
   }
 
-
+  const debouncedSearch = _debounce(handleSearch, 1500);
 
   return (
     <>
-      <form onSubmit={ handleSubmit }>
-        <input type="text" placeholder="Search" name='query'/>
-        <button type="submit">Search</button>
-      </form>
+      <input type="text" placeholder="Search" onChange={ (e) => debouncedSearch(e.target.value) }/>
       <div>
-        { res.map((item, index) => {
+        { loading ? (
+          <p>Loading...</p> 
+        ) : (
+          res.map((item, index) => {
           return (
             <div key={index}>
-              <h3>{item.volumeInfo.title}</h3>
+              <h3>Title: {item.volumeInfo.title}</h3>
               <p>{item.volumeInfo.author}</p>
-              <img src={item.volumeInfo.imageLinks.thumbnail} alt={item.volumeInfo.title} />
+              <img src={item.volumeInfo.imageLinks?.smallThumbnail} alt={item.volumeInfo.title} />
             </div>
           )
-        })}
+        }))}
       </div>
     </>
   )
